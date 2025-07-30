7c4a91ae5ee0c47eaa5176f831b6bfcdab2b75ac
Artem <wannacry1664@gmail.com>
2025-07-30 17:17:43 +0300

    feat: add full adaptive overlay menu

--- museum/scripts/header.js
+++ museum/scripts/header.js
@@ -1,5 +1,8 @@
 const burgerButton = document.querySelector('.header__burger-button');
 const headerList = document.querySelector('.header__list');
+const headerOverlay = document.querySelector('.header__overlay');
+const headerPicture = document.querySelector('.header__nav-picture');
+const headerSocial = document.querySelector('.header__social');
 const welcomeTitle = document.querySelector('.welcome__title');
 const welcomeDescr = document.querySelector('.welcome__descr');
 const welcomeButton = document.querySelector('.welcome__button');
@@ -8,7 +11,9 @@ const breakPoint = 1024;
 
 function toggleMenu() {
   burgerButton.classList.toggle('is-active');
-  headerList.classList.toggle('is-active');
+  headerOverlay.classList.toggle('is-active');
+  headerPicture.classList.toggle('is-active');
+  headerSocial.classList.toggle('is-active');
   welcomeTitle.classList.toggle('overlay--hidden');
   welcomeDescr.classList.toggle('overlay--hidden');
   welcomeButton.classList.toggle('overlay--hidden');
@@ -18,7 +23,9 @@ function toggleMenu() {
 function resetMenuForDesktop() {
   if (window.innerWidth >= breakPoint) {
     burgerButton.classList.remove('is-active');
-    headerList.classList.remove('is-active');
+    headerOverlay.classList.remove('is-active');
+    headerPicture.classList.remove('is-active');
+    headerSocial.classList.remove('is-active');
     welcomeTitle.classList.remove('overlay--hidden');
     welcomeDescr.classList.remove('overlay--hidden');
     welcomeButton.classList.remove('overlay--hidden');
